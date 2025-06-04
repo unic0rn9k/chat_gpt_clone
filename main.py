62f3565bcbec4f2d7beb18afbffe5a3d2d1b98b5
unic0rn9k <unic0rn9k@protonmail.com>
2025-06-04 12:25:03 +0200

    insert messages

--- main.py
+++ main.py
@@ -3,13 +3,35 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from llama_index.llms.ollama import Ollama
 from fastapi import FastAPI
-import asyncio
 from concurrent.futures import ThreadPoolExecutor
+import pandas as pd
+from sqlalchemy import create_engine
+import psycopg2
+from psycopg2 import pool
+from fastapi import FastAPI, Depends
+import datetime as dt
+from psycopg2.extras import execute_values
 
-app = FastAPI()
+app = FastAPI(debug=True)
 executor = ThreadPoolExecutor(max_workers=10)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+DATABASE_URL = "postgresql://postgres:postgres@postgres:5432/mydb"
+connection_pool = psycopg2.pool.SimpleConnectionPool(
+    minconn=1,
+    maxconn=10,
+    dsn=DATABASE_URL
+)
+
+def get_db_conn():
+    conn = None
+    try:
+        conn = connection_pool.getconn()
+        yield conn
+    finally:
+        if conn:
+            connection_pool.putconn(conn)
 
 @app.get("/", response_class=HTMLResponse)
 async def get_chat_page():
@@ -17,21 +39,30 @@ async def get_chat_page():
         return file.read()
 
 @app.get("/chat/{message}")
-async def chat(message: str):
-    llm = Ollama(model="gemma3:1b", request_timeout=240.0, base_url="ollama:11434")
-    response = llm.complete(message, timeout=None)
+async def chat(message: str, conn=Depends(get_db_conn)):
+    with conn.cursor() as cur:
+        cur.execute("SELECT COUNT(*) FROM messages;")
+        msg_count = cur.fetchone()[0]
+
+        now = dt.datetime.now()
+
+        llm = Ollama(model="qwen3:0.6b", request_timeout=240.0, base_url="ollama:11434")
+        response = f"{llm.complete(message, timeout=None)}"
+
+        data = [
+            ("user", msg_count, message, now, 0),
+            ("bot", msg_count, response, now, 0)
+        ]
+
+        insert_query = """
+            INSERT INTO messages (author, id, content, timestamp, chat_id)
+            VALUES %s
+        """
+
+        execute_values(cur, insert_query, data)
+    conn.commit()
+
     return response
-
-# --------------------------------------------------------------------------
-
-from typing import Union
-import pandas as pd
-from sqlalchemy import create_engine
-import time
-import psycopg2
-import os
-
-#app = FastAPI()
 
 @app.get("/initialize")
 async def initialize():
@@ -59,8 +90,8 @@ async def initialize():
             );
             
             CREATE TABLE messages (
-                id INT,
                 author VARCHAR(255),
+                id INT NOT NULL,
                 chat_id INT NOT NULL,
                 content TEXT,
                 timestamp TIMESTAMP,
